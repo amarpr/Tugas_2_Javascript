259e381dbfa4cb4ec3b8e512a2423ec1db86f7fb
Muhamar <amararrasyid@gmail.com>
2020-05-03 12:48:50 +0700

    Tugas 2 Javascript
    Mengumpulkan Tugas Niomic

---
+++ index.js
@@ -0,0 +1,11 @@
+var number = 2;
+var string = "Muhamar";
+var boolean = true;
+var nl = null;
+var undfn;
+
+console.log(number)
+console.log(string)
+console.log(boolean)
+console.log(nl)
+console.log(undfn)
